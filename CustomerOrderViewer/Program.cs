4ac307e534b58e6dc554df4722d9abd118dc55e2
Asus <Asus@DESKTOP-E94IUGD>
2023-03-20 15:21:04 +0100

    Add project files.

---
+++ CustomerOrderViewer/Program.cs
@@ -0,0 +1,38 @@
+﻿using CustomerOrderViewer.Models;
+using CustomerOrderViewer.Repository;
+using System;
+using System.Data.SqlClient;
+
+namespace CustomerOrderViewer
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            try
+            {
+                CustomerOrderDetailCommand customerOrderDetailCommand = new CustomerOrderDetailCommand(@"Data Source=localhost;Initial Catalog=CustomerOrderView;Integrated Security=True");
+                IList<CustomerOrderDetailModel> customerOrderDetailModels = customerOrderDetailCommand.GetList();
+
+                if (customerOrderDetailModels.Any())
+                {
+                    foreach(CustomerOrderDetailModel customerOrderDetailModel in customerOrderDetailModels)
+                    {
+                        Console.WriteLine("{0}: FullName: {1} {2} (Id: {3}) - purchased {4} for {5} (Id: {6})",customerOrderDetailModel.CustomerOrderId,
+                            customerOrderDetailModel.FirstName,
+                            customerOrderDetailModel.LastName,
+                            customerOrderDetailModel.CustomerId,
+                            customerOrderDetailModel.Description,
+                            customerOrderDetailModel.Price,
+                            customerOrderDetailModel.ItemId);    
+                    }
+                }
+            }  
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ CustomerOrderViewer/Repository/CustomerOrderDetailCommand.cs
@@ -0,0 +1,55 @@
+﻿using CustomerOrderViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrderViewer.Repository
+{
+    internal class CustomerOrderDetailCommand
+    {
+        private string connectionString;
+
+        public CustomerOrderDetailCommand(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+        public IList<CustomerOrderDetailModel> GetList()
+        {
+            List<CustomerOrderDetailModel> customerOrderDetailModels = new List<CustomerOrderDetailModel>();
+
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using(SqlCommand command = new SqlCommand("SELECT CustomerOrderId, CustomerId, ItemId, Firstname, Lastname, [Description], Price FROM CustomerOrderDetail",connection))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                CustomerOrderDetailModel customerOrderDetailModel = new CustomerOrderDetailModel()
+                                {
+                                    CustomerOrderId = Convert.ToInt32(reader["CustomerOrderId"]),
+                                    CustomerId = Convert.ToInt32(reader["CustomerId"]),
+                                    ItemId = Convert.ToInt32(reader["ItemId"]),
+                                    FirstName = reader["FirstName"].ToString(),
+                                    LastName = reader["LastName"].ToString(),
+                                    Description = reader["Description"].ToString(),
+                                    Price = Convert.ToDecimal(reader["Price"])
+                                };
+                                customerOrderDetailModels.Add(customerOrderDetailModel);
+                                var firstname = reader["Firstname"].ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return customerOrderDetailModels;
+        }
+    }
+}
